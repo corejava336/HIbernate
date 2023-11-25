785249c2bc824ee93a25d47ad1e59396a97d0304
Dell <Dell@DESKTOP-DSDD3MM>
2023-11-25 16:46:36 +0530

    HB_9

---
+++ Hb_9_Project_Con_Pool_Vibur/lib/src/main/java/com/nt/entity/Bank.java
@@ -0,0 +1,67 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
+import antlr.Version;
+
+public class Bank implements Serializable 
+{
+
+private static final long serialVersionUID = 1L;
+private int aid;
+private String name;
+private double balance;
+private String type;
+private LocalDateTime DateOfOpen;
+private Timestamp LastUpdated;
+public int getAid() {
+	return aid;
+}
+public void setAid(int aid) {
+	this.aid = aid;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public double getBalance() {
+	return balance;
+}
+public void setBalance(double balance) {
+	this.balance = balance;
+}
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public LocalDateTime getDateOfOpen() {
+	return DateOfOpen;
+}
+public void setDateOfOpen(LocalDateTime dateOfOpen) {
+	DateOfOpen = dateOfOpen;
+}
+public Timestamp getLastUpdated() {
+	return LastUpdated;
+}
+public void setLastUpdated(Timestamp lastUpdated) {
+	LastUpdated = lastUpdated;
+}
+public static long getSerialversionuid() {
+	return serialVersionUID;
+}
+@Override
+public String toString() {
+	return "Bank [aid=" + aid + ", name=" + name + ", balance=" + balance + ", type=" + type + ", DateOfOpen="
+			+ DateOfOpen + ", LastUpdated=" + LastUpdated + "]";
+}
+
+}
